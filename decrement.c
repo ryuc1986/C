c571aca491335473c76904d05ae72465a7e8398b
ryuc1986 <cho4649diver@yahoo.co.jp>
2016-12-03 23:15:18 +0900

    decrement
    practice2

---
+++ decrement.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello, world\n");
+	return 0;
+} Integer : ");	scanf("%d", &num);
+	
+	if (num >=1){
+		
+		int i = 2;
+		while(i <= num){
+			printf("%d\n",i);
+			i += 2;
+		}
+	}
+	return 0;
+}
+
